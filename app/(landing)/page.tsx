92c7dca71e214d5f30e62a3320bc849710b8a08c
Royer Gaétan <royergaetan90@gmail.com>
2023-11-13 20:29:33 +0100

    add routes, layout and clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const LandingPage = () => {
-  return <div>Landing Page (Protected)</div>;
+  return (
+    <div>
+      Landing Page
+      <div>
+        <Link href="/sign-in">
+          <Button>Connexion</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Inscription</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
