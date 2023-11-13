bcfe82f1b0e86ec912bd6115ae29586150462de7
Royer Gaétan <royergaetan90@gmail.com>
2023-11-14 00:06:49 +0100

    add dashboard

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Menu } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Sidebar from '@/components/sidebar';
+
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="md:hidden" />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
