136ae01d4673dabbf9955a231bbaaa3478a0d19a
Royer Gaétan <royergaetan90@gmail.com>
2023-11-15 20:22:30 +0100

    generate code + generate img + format conversation

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import * as z from 'zod';
-import { MessageSquare } from 'lucide-react';
+import { Code } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
@@ -17,11 +17,13 @@ import { Empty } from '@/components/empty';
 import { Loader } from '@/components/loader';
 import { cn } from '@/lib/utils';
 import { UserAvatar } from '@/components/user-avatar';
+import { BotAvatar } from '@/components/bot-avatar';
+import ReactMarkdown from 'react-markdown';
 
 import { formSchema } from './constants';
-import { BotAvatar } from '@/components/bot-avatar';
+import { Autour_One } from 'next/font/google';
 
-const ConversationPage = () => {
+const CodePage = () => {
   const router = useRouter();
   const [messages, setMessages] = useState<ChatCompletionMessageParam[]>([]);
 
@@ -42,9 +44,11 @@ const ConversationPage = () => {
       };
       const newMessages = [...messages, userMessage];
 
-      const response = await axios.post('/api/conversation', {
+      const response = await axios.post('/api/code', {
         messages: newMessages,
       });
+
+      console.log(messages);
 
       setMessages((current) => [...current, userMessage, response.data]);
 
@@ -60,11 +64,11 @@ const ConversationPage = () => {
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Notre modèle de conversation le plus avancé."
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Génération de Code"
+        description="Générer du code à l'aide de texte descriptif."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
       <div className="px-4 lg:px-8">
         <div>
@@ -81,7 +85,7 @@ const ConversationPage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="Qui a découvert la banane ?"
+                        placeholder="Bouton bascule simple utilisant les hooks React."
                         {...field}
                       />
                     </FormControl>
@@ -104,7 +108,7 @@ const ConversationPage = () => {
             </div>
           )}
           {messages.length === 0 && !isLoading && (
-            <Empty label="Aucune conversation initiée." />
+            <Empty label="Aucun code initié." />
           )}
 
           <div className="flex flex-col-reverse gap-y-4">
@@ -119,7 +123,22 @@ const ConversationPage = () => {
                 )}
               >
                 {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content as string}</p>
+
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {(message.content as string) || ''}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
@@ -129,4 +148,4 @@ const ConversationPage = () => {
   );
 };
 
-export default ConversationPage;
+export default CodePage;
